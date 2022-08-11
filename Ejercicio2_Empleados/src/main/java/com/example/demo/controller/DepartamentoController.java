bd137e56e47717d031814ef9b08b5a2e570e3ca5
IzanDuesoLopez <izandueso@gmail.com>
2022-08-11 12:32:26 +0200

    DepartamentoController

--- Ejercicio2_Empleados/src/main/java/com/example/demo/controller/DepartamentoController.java
+++ Ejercicio2_Empleados/src/main/java/com/example/demo/controller/DepartamentoController.java
@@ -3,9 +3,11 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +37,36 @@ public class DepartamentoController {
 	// Posteamos un departamento
 	@PostMapping("/departamentos")
 	public Departamento guardarDepartamento(@RequestBody Departamento departamento) {
+		return departamentoServiceImpl.guardarDepartamento(departamento);
+	}
+	
+	// Listamos departamentos por codigo
+	@GetMapping("/departamentos/{codigo}")
+	public Departamento departamentoXCodigo(@PathVariable(name="codigo") Long codigo) {
+		Departamento departamento_x_id = new Departamento();
+		departamento_x_id = departamentoServiceImpl.departamentoXCodigo(codigo);
+		return departamento_x_id;
+	}
+	
+	// Actualizamos departamentos por ID
+	@PutMapping("/departamentos/{codigo}")
+	public Departamento actualizarDepartamento(@PathVariable(name = "codigo") Long codigo, @RequestBody Departamento departamento) {
+		Departamento departamento_seleccionado = new Departamento();
+		Departamento departamento_actualizado = new Departamento();
 		
+		departamento_seleccionado = departamentoServiceImpl.departamentoXCodigo(codigo);
+		
+		departamento_seleccionado.setNombre(departamento.getNombre());
+		departamento_seleccionado.setPresupuesto(departamento.getPresupuesto());
+		
+		departamento_actualizado = departamentoServiceImpl.actualizarDepartamento(departamento_seleccionado);
+		
+		return departamento_actualizado;
+	}
+	
+	// Eliminar departamento por codigo
+	@DeleteMapping("/departamentos/{codigo}")
+	public void eliminarDepartamento(@PathVariable(name = "codigo") Long codigo) {
+		departamentoServiceImpl.eliminarDepartamento(codigo);
 	}
 }
