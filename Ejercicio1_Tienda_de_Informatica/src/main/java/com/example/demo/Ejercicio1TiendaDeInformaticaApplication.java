f828f2e69d4481df5a7d11875c94fb614edf2b38
IzanDuesoLopez <izandueso@gmail.com>
2022-08-11 09:14:37 +0200

    Initial push origin main

---
+++ Ejercicio1_Tienda_de_Informatica/src/main/java/com/example/demo/Ejercicio1TiendaDeInformaticaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio1TiendaDeInformaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio1TiendaDeInformaticaApplication.class, args);
+	}
+
+}
