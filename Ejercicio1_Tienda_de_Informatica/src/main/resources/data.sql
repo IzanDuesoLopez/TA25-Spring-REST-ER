1f5897e0abaa5ff30675deb03ee2b989f5a3a4c3
IzanDuesoLopez <izandueso@gmail.com>
2022-08-11 10:55:44 +0200

    Cambios finales SQL

--- Ejercicio1_Tienda_de_Informatica/src/main/resources/data.sql
+++ Ejercicio1_Tienda_de_Informatica/src/main/resources/data.sql
@@ -14,4 +14,12 @@ create table articulos(
 );
 
 insert into Fabricantes(nombre)values('Intel');
-insert into Articulos(nombre, precio, fabricante)values('i9 12900k', 500, 1);
+insert into Fabricantes(nombre)values('NVidia');
+insert into Fabricantes(nombre)values('AMD');
+insert into Fabricantes(nombre)values('CoolerMaster');
+insert into Fabricantes(nombre)values('Corsair');
+insert into Articulos(nombre, precio, fabricante)values('i9_12900k', 500, 1);
+insert into Articulos(nombre, precio, fabricante)values('RTX_3090ti', 2600, 2);
+insert into Articulos(nombre, precio, fabricante)values('Ryzen_9_5950X', 750, 3);
+insert into Articulos(nombre, precio, fabricante)values('CM_HP500', 250, 4);
+insert into Articulos(nombre, precio, fabricante)values('RMX_750W', 199, 5);
