2a942039dd8d6a51bce5ac99fd7fe3d82866520c
IzanDuesoLopez <izandueso@gmail.com>
2022-08-12 11:40:45 +0200

    Inserts sql

--- Ejercicio2_Empleados/src/main/resources/data.sql
+++ Ejercicio2_Empleados/src/main/resources/data.sql
@@ -17,4 +17,12 @@ create table empleados (
 );
 
 insert into departamentos(nombre, presupuesto)values('Reus', 2000);
+insert into departamentos(nombre, presupuesto)values('Tarragona', 250);
+insert into departamentos(nombre, presupuesto)values('Salou', 5678);
+insert into departamentos(nombre, presupuesto)values('Vila-Seca', 3500);
+insert into departamentos(nombre, presupuesto)values('Cambrils', 1290);
 insert into empleados(dni, nombre, apellidos, departamento)values('12345678', 'Pepe', 'Pepi', 1);
+insert into empleados(dni, nombre, apellidos, departamento)values('87654321', 'Izan', 'Dueso', 2);
+insert into empleados(dni, nombre, apellidos, departamento)values('11112222', 'Joel', 'Romero', 3);
+insert into empleados(dni, nombre, apellidos, departamento)values('33334444', 'EquipoTres', 'Interviews', 4);
+insert into empleados(dni, nombre, apellidos, departamento)values('55556666', 'Logan', 'Law', 5);
