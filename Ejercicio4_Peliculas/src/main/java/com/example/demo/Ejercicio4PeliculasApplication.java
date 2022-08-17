fdaf5fd1a4273cbb20d902bac88cbcf904394ae2
IzanDuesoLopez <izandueso@gmail.com>
2022-08-17 11:35:17 +0200

    Ejercicio 4 inicio

---
+++ Ejercicio4_Peliculas/src/main/java/com/example/demo/Ejercicio4PeliculasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio4PeliculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio4PeliculasApplication.class, args);
+	}
+
+}
---
+++ Ejercicio4_Peliculas/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+DROP table IF EXISTS peliculas, salas;
+
+create table peliculas(
+	codigo int auto_increment primary key,
+    nombre nvarchar(100),
+    edad int
+);
+
+create table salas(
+	codigo int auto_increment primary key,
+    codigo_pelicula int,
+    nombre nvarchar(100),
+    constraint fk_sala foreign key(codigo_pelicula) references peliculas(codigo)
+);
+
+insert into peliculas(nombre, edad)values('Marvels Spider-man', 12);
+insert into salas(codigo_pelicula, nombre)values(1, 'Sala1');
